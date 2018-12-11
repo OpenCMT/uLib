e8b99dd0d53c8991f859a2c6aa16df88db4e3d66
Paolo Andreetto <paolo.andreetto@pd.infn.it>
2018-12-11 14:19:23 +0100

    CMake module for rpms

---
+++ CMake/uLibGenerateRPMS.cmake
@@ -0,0 +1,34 @@
+# Target for RPMs creation
+
+set(PKGREL 1)
+set(ITEMS_FOR_SOURCE AUTHORS
+                     CMakeConfig.in.h
+                     CMakePkgConfig.pc.in
+                     test.cpp
+                     uLibConfig.cmake.in
+                     uLib.creator
+                     ulib.h
+                     vtk_test.cpp
+                     CMake
+                     CMakeLists.txt
+                     Jenkinsfile
+                     src
+                     uLib.config
+                     uLibConfigVersion.cmake.in
+                     uLib.files
+                     uLib.includes)
+
+add_custom_target(rpm 
+                  COMMAND mkdir -p ${CMAKE_CURRENT_BINARY_DIR}/rpmbuild/BUILD
+                                   ${CMAKE_CURRENT_BINARY_DIR}/rpmbuild/RPMS
+                                   ${CMAKE_CURRENT_BINARY_DIR}/rpmbuild/SOURCES
+                                   ${CMAKE_CURRENT_BINARY_DIR}/rpmbuild/SPECS
+                                   ${CMAKE_CURRENT_BINARY_DIR}/rpmbuild/SRPMS
+                  COMMAND tar -zcf ${CMAKE_CURRENT_BINARY_DIR}/rpmbuild/SOURCES/cmt-ulib-${PACKAGE_VERSION}.tar.gz ${ITEMS_FOR_SOURCE}
+                  COMMAND sed -e 's|@PKGVERSION@|${PACKAGE_VERSION}|g'
+                              -e 's|@PKGRELEASE@|${PKGREL}|g' 
+                              CMake/cmt-ulib.spec.in > ${CMAKE_CURRENT_BINARY_DIR}/rpmbuild/SPECS/cmt-ulib.spec
+                  COMMAND QA_SKIP_BUILD_ROOT=1 rpmbuild -ba --define '_topdir ${CMAKE_CURRENT_BINARY_DIR}/rpmbuild'
+                          ${CMAKE_CURRENT_BINARY_DIR}/rpmbuild/SPECS/cmt-ulib.spec
+                  WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR})
+
